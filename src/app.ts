5c49e01e06e532e86af0c9c1332560a74eee0c5e
stormzy22 <aawweb23@gmail.com>
2021-07-18 10:59:39 +0100

    Done

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
-const greeting: string = "Hello";
+import express, { Application, Request, Response, NextFunction } from "express";
 
-const numbers: number[] = [1, 2, 3];
+const app: Application = express();
+
+const add = (a: number, b: number): number => a + b;
+
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  console.log(add(5, 5));
+
+  res.send("Hello");
+});
+
+app.listen(5000, () => console.log("Server running "));
